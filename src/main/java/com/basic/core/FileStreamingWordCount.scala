252b96f1cedcc612aa4642fc413504df5d7c6f0c
dell-pc <798750509@qq.com>
2017-04-15 01:59:24 +0800

    2017/4/15 1:59

--- src/main/java/com/basic/core/FileStreamingWordCount.scala
+++ src/main/java/com/basic/core/FileStreamingWordCount.scala
@@ -1,6 +1,11 @@
 package com.basic.core
 
+import java.io.FileInputStream
+
 import com.basic.util.PropertiesUtils
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.io.{LongWritable, Text}
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat
 import org.apache.spark.SparkConf
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.{Seconds, StreamingContext}
@@ -24,13 +29,18 @@ object FileStreamingWordCount {
     conf.setAppName("SprakStreamingWordCount") //设置应用程序的名称，在程序运行的监控界面可以看到名称
 
     //每隔1秒计算一批数据
-    val ssc=new StreamingContext(conf,Seconds(1))
+    val ssc=new StreamingContext(conf,Seconds(10))
+    var filePath=args(0)
+    //从HDFS中读取文件.map(_._2.toString)
 
-    //从HDFS中读取文件
-    val lines=ssc.textFileStream(args(0))
+//    val filter = (name:Path)=>true
+//
+//    val lines=ssc.fileStream[LongWritable, Text, TextInputFormat](filePath,filter,false).map(a=>a._2.toString)
+    val lines=ssc.textFileStream(filePath)
+    //val lines=ssc.fileStream[LongWritable, Text, TextInputFormat](filePath, (t: Path) => true, false).map(_._2.toString)
 
     val words=lines.flatMap(_.split(" "))
-    val pairs=words.map(word=>(word,1))
+    val pairs=lines.map(word=>(word,1))
     val wordCounts=pairs.reduceByKey(_+_)
 
     //    //排序结果集打印，先转成rdd，然后排序true升序，false降序，可以指定key和value排序_._1是key，_._2是value
